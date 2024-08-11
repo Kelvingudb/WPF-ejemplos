dbcdf90ec32690193f0c140a6b84d818d64bd460
LuisOrt1z <OH242206@alumno.udb.edu.sv>
2024-08-11 14:49:40 -0600

    ejemplos edicion de texto

---
+++ OH242206_ejemplos_dspteorico/controles_de_edicion_de_texto/Window2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace controles_de_edicion_de_texto
+{
+    /// <summary>
+    /// Lógica de interacción para Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void BoldButton_Click(object sender, RoutedEventArgs e)
+        {
+            SimpleRichTextBox.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+        }
+
+        private void ItalicButton_Click(object sender, RoutedEventArgs e)
+        {
+            SimpleRichTextBox.Selection.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Italic);
+        }
+    }
+}
